b96182087b8a085ba1c4f6b1f935b6ea91fbb038
Dany Christian <45038103+MSerperior@users.noreply.github.com>
2025-10-21 15:49:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jds_pretest
+
---
+++ app/Models/News.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    /** @use HasFactory<\Database\Factories\NewsFactory> */
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'news_title',
+        'news_content',
+        'news_image',
+        'news_slug'
+    ];
+}
