1c5a692725586f18325226410d941bf4c92d1cec
Dany Christian <45038103+MSerperior@users.noreply.github.com>
2025-10-27 13:34:36 +0700

    refactor : move news image and add temporary url

--- app/Http/Resources/NewsResource.php
+++ app/Http/Resources/NewsResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class NewsResource extends JsonResource
 {
@@ -18,7 +19,7 @@ class NewsResource extends JsonResource
             'id' => $this->uuid,
             'news_title' => $this->news_title,
             'news_content' => $this->news_content,
-            'news_image' => $this->news_image,
+            'news_image' => $this->news_image ? (str_starts_with($this->news_image, 'http') ? $this->news_image : Storage::disk('local')->temporaryUrl($this->news_image, now()->addMinutes(5))) : null,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
 
